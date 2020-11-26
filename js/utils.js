5a878e423f059c9683229b130301764a7496b44e
ebraraktas <mustafa.eaktas@gmail.com>
2020-11-26 23:00:55 +0300

    Add some date utility functions and documentation

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,21 @@
 function getDateFormatted(dateObj, sep) {
+    // Convert Date object to string joined by `sep`. For example, if `sep='.'` string will be dd.mm.yyyy
     return [(dateObj.getDate() < 10 ? '0' : '') + dateObj.getDate(), (dateObj.getMonth() < 9 ? '0' : '') + (dateObj.getMonth() + 1), dateObj.getFullYear()].join(sep)
+}
+
+function stringToDateArray(dateString) {
+    // Parse dateString in dd.mm.yyyy format to array of [year, month, day]
+    return dateString.split('.').reverse().map(x=>+x);
+} 
+
+function compareDateStrings(dateString1, dateString2) {
+    // Return dateString1 < dateString2, assuming date format is dd.mm.yyyy
+    let dateArray1 = stringToDateArray(dateString1);
+    let dateArray2 = stringToDateArray(dateString2);
+    for (var i = 0; i < 3; i++) {
+        if (i < 2 && dateArray1[i] == dateArray2[i]) {
+            continue;
+        } 
+        return dateArray1[i] < dateArray2[i];
+    }
 }
