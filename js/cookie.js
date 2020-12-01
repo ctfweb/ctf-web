f944d583edbe489e821b74e18764f182c80e1384
ebraraktas <mustafa.eaktas@gmail.com>
2020-12-01 22:57:57 +0300

    Set default cookie expiration date to 180 days

--- js/cookie.js
+++ js/cookie.js
@@ -1,4 +1,4 @@
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays=180) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires=" + d.toUTCString();
